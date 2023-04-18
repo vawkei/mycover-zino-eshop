bb25a465d3adc9f7a4ffa9b3ad66aebcaad742ac
voke sokoh <vokes9810@gmail.com>
2023-04-18 09:51:30 +0800

    Secured my firebaseConfig data

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -10,12 +10,12 @@ import {getFirestore} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD-RevQ1yK9P2kM5mkAa2dcvZ4VyWYORYc",
-  authDomain: "my-eshop-cover.firebaseapp.com",
-  projectId: "my-eshop-cover",
-  storageBucket: "my-eshop-cover.appspot.com",
-  messagingSenderId: "330590864478",
-  appId: "1:330590864478:web:97c3f84d1a9385ffcbe0cf"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
